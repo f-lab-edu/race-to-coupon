49017c9f0954926e0df08bf2627ae99aa6205615
SpearZero <spearzero0@gmail.com>
2025-07-01 18:57:39 +0900

    init: README.md 수정 및 Spring Rest Docs 관련 설정 추가

---
+++ src/test/java/com/rtc/app/sample/SampleControllerTest.java
@@ -0,0 +1,42 @@
+package com.rtc.app.sample;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("샘플 컨트롤러 REST 테스트")
+@SpringBootTest
+@ExtendWith(RestDocumentationExtension.class)
+class SampleControllerTest {
+
+    @Autowired private WebApplicationContext context;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp(RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+                .apply(documentationConfiguration(restDocumentation)).build();
+    }
+
+    @DisplayName("GET /sample")
+    @Test
+    void getSample() throws Exception {
+        this.mockMvc
+                .perform(get("/sample"))
+                .andExpect(status().isOk())
+                .andDo(document("sample"));
+    }
+}
